3fc02efc285429856abb172cf6ba8a42ecb45e57
Thomas Dornberger <ga84bib@mytum.de>
2018-12-03 23:13:08 +0100

    use directories class with constants, remove unused code

--- src/data_export/PitMergedMutationsWriter.java
+++ src/data_export/PitMergedMutationsWriter.java
@@ -9,37 +9,39 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+import directories.globals.Directories;
 import pit.data_objects.PitMethod;
-import pit.data_objects.PitTestCase;
 
 public class PitMergedMutationsWriter {
-	public static final String MERGED_METHODS_FILE_NAME = "methods-and-mutations-merged.xml";
-	public static final String MERGED_TESTS_FILE_NAME = "tests-merged.xml";
+//	public static final String MERGED_TESTS_FILE_NAME = "tests-merged.xml";
 	
 	public static void writeMergedMethodsAndMutationsFile(String outputDir, List<PitMethod> methods) throws IOException {
-		String outputFile = outputDir + MERGED_METHODS_FILE_NAME;
+		String outputFile = outputDir + Directories.MERGED_METHODS_FILE_NAME;
 		Path outputFilePath = Paths.get(outputFile);
 		Files.deleteIfExists(outputFilePath);
 		
-		System.out.println("INFO: Writing all methods to file " + outputDir + MERGED_METHODS_FILE_NAME);
-		FileOutputStream fos = new FileOutputStream(new File(outputDir + MERGED_METHODS_FILE_NAME));
+		System.out.println("INFO: Writing all methods to file " + outputDir + Directories.MERGED_METHODS_FILE_NAME);
+		FileOutputStream fos = new FileOutputStream(new File(outputDir + Directories.MERGED_METHODS_FILE_NAME));
 		XMLEncoder encoder = new XMLEncoder(fos);
 		encoder.writeObject(methods);
 		encoder.close();
 		fos.close();
 	}
-	public static void writeAllTestsFile(String outputDir, List<PitTestCase> tests) throws IOException {
-		String outputFile = outputDir + MERGED_TESTS_FILE_NAME;
-		Path outputFilePath = Paths.get(outputFile);
-		Files.deleteIfExists(outputFilePath);
-		
-		System.out.println("INFO: Writing all tests to file " + outputDir + MERGED_TESTS_FILE_NAME);
-		FileOutputStream fos = new FileOutputStream(new File(outputDir + MERGED_TESTS_FILE_NAME));
-		XMLEncoder encoder = new XMLEncoder(fos);
-		for (PitTestCase test: tests) {
-			encoder.writeObject(test);
-		}
-		encoder.close();
-		fos.close();
-	}
+	/**
+	 * Unused.
+	 */
+//	public static void writeAllTestsFile(String outputDir, List<PitTestCase> tests) throws IOException {
+//		String outputFile = outputDir + MERGED_TESTS_FILE_NAME;
+//		Path outputFilePath = Paths.get(outputFile);
+//		Files.deleteIfExists(outputFilePath);
+//		
+//		System.out.println("INFO: Writing all tests to file " + outputDir + MERGED_TESTS_FILE_NAME);
+//		FileOutputStream fos = new FileOutputStream(new File(outputDir + MERGED_TESTS_FILE_NAME));
+//		XMLEncoder encoder = new XMLEncoder(fos);
+//		for (PitTestCase test: tests) {
+//			encoder.writeObject(test);
+//		}
+//		encoder.close();
+//		fos.close();
+//	}
 }
